529cacdf6656d46eed8187483cfc0821cf0af1da
Manas Ranjan Sahoo <manasarmo.1819r@gmail.com>
2026-01-10 04:53:14 +0530

    env added

--- constants.py
+++ constants.py
@@ -1,6 +1,13 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+import os
+
 ZOMATO_URL = "https://www.zomato.com/bangalore/dominos-pizza-shanti-nagar/order"
 
-EMAIL_ID = "manasarmo.1819r@gmail.com"
-PHONE_NUMBER = "7377229622"
+EMAIL_ID = os.environ.get("EMAIL")
+PHONE_NUMBER = os.environ.get("PHONE_NUMBER")
 
 OTP_WAIT_TIME = 360  # seconds
+
+
